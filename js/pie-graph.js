e45d7294a377dd0e9e738788f26204e511a915d9
Steven De Avila <stevejd88@gmail.com>
2019-07-26 11:13:10 -0500

    created pie chart

---
+++ js/pie-graph.js
@@ -0,0 +1,28 @@
+var ctx = document.getElementById('pieGraph').getContext('2d');
+// For a pie chart
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [25, 15, 60],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderAlign: 'inner',
+            borderColor: 'rgba(0, 0, 0,0)',
+            weight: 1,
+        }],
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
+        ]
+    },
+    options: {
+        circumference: 2* Math.PI,
+        // cutOutPercentage: 50%
+    }
+});
