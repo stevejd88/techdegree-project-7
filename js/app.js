09cf422e7e66845a1d46b1ff7051774e18d34230
Steven De Avila <stevejd88@gmail.com>
2019-07-31 09:57:56 -0500

    created alert popup in js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const alertPopUp = document.getElementById('alertPopUp');
+// const btnAlert = document.;
+
+alertPopUp.innerHTML =
+`
+<div class=alert-banner>
+  <div class="alert-wrapper">
+    <h3>Alert!</h3>
+    <p>This is an alert message</p>
+  </div>
+  <div class="btn-wrapper">
+    <button class="btn-alert" type=button name=button>&times;</button>
+  </div>
+</div>
+`;
