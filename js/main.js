1963e877d57f3d22bc05542c2731b1a9cde8e8d7
Steven De Avila <stevejd88@gmail.com>
2019-08-01 12:19:38 -0500

    updated message form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const alertPopUp = document.getElementById('alertPopUp');
 const btnAlert = document.querySelector('.btn-alert');
+const headerAlert = document.querySelector('.notification');
+const messageForm = document.getElementById('messageForm');
 
 //create pop up alert div
 alertPopUp.innerHTML =
@@ -25,3 +27,56 @@ const displayNone = (el, value) => {
   });
 }
 displayNone(alertPopUp, 'btn-alert');
+
+
+
+// Name and Email validation Function.
+function validation(){
+  const name = document.querySelector(".user-search").value;
+  const message = document.querySelector(".search-text").value;
+
+  if( name ==='') {
+    alert("Please fill in the user name...!!!!!!");
+    return false;
+  } else if( message ==='') {
+    alert("Please type your message...!!!!!!");
+    return false;
+   }else {
+    return true;
+  }
+}
+
+
+// Submit form with id function.
+function formSubmit() {
+  if (validation()) {
+    messageForm.submit(); //form submission
+    alert("Form Submitted Successfully......");
+  }
+}
+
+messageForm.addEventListener('click', e => {
+  const element = e.target;
+  if(element.className === 'user-search-btn') {
+    formSubmit();
+    messageForm.reset();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+// // Header Icon Alert Popup
+// headerAlert.addEventListener('click', e => {
+// const unread = document.createElement('div');
+//   for (let i = 0; i < 4; i++) {
+//     if (headerAlert.className === 'notification')
+//     headerAlert.appendChild(unread);
+//   }
+// });
